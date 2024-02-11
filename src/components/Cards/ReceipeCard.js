a97006517bcc9a82e549d1f9485f46beceadcc2b
Sourav0809 <spathak7431@gmail.com>
2024-02-12 02:40:50 +0530

    Receipe card added with dummy data

--- src/components/Cards/ReceipeCard.js
+++ src/components/Cards/ReceipeCard.js
@@ -1,8 +1,29 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
+import { IoLocationOutline } from "react-icons/io5";
 
 const ReceipeCard = () => {
     return (
-        <div>ReceipeCard</div>
+        <Box sx={
+            {
+                width: '348px',
+                height: "140px",
+                borderRadius: "12px",
+                display: 'flex',
+                flexDirection: "column",
+                gap: "10px",
+                padding: "15px",
+                backgroundColor: "white",
+                border: '1px solid #E5E5E5'
+            }}>
+            <IoLocationOutline style={{ fontSize: "48px" }} />
+            <Typography sx={{ fontSize: '24px', opacity: '0.7' }}>
+                Receipe Management
+            </Typography>
+            <Typography sx={{ fontSize: '12px', opacity: '0.7', display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
+                Manage
+            </Typography>
+        </Box>
     )
 }
 
